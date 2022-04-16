b20a3a2821d9f3530ec1db98c93cbe8d5a5271ee
hedwig100 <pentagon7951@outlook.jp>
2022-04-16 14:03:08 +0900

    モジュールをインポートするための__init__.pyを追加,weatherのリファクタリング

--- src/weather.py
+++ src/weather.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import requests
 from datetime import datetime
+import requests
 
 def confirm_aqc(data:list) -> str:
     """
@@ -13,7 +13,6 @@ def confirm_aqc(data:list) -> str:
     else:
         return "品質情報を確認して下さい"
 
-
 def find_index(data:list, code:str) -> int:
     """
     対象のエリアのデータが格納されているインデックス番号を返す
@@ -24,6 +23,14 @@ def find_index(data:list, code:str) -> int:
     return index
 
 def weather():
+    """
+    天気に関するデータを返す
+
+    Returns:
+        str: 当日の天気
+        str: 翌日の天気
+        float: 当日の気温
+    """
     latest_time_url = "https://www.jma.go.jp/bosai/amedas/data/latest_time.txt"
     latest_time_req = requests.get(latest_time_url)
     latest_datetime = datetime.strptime(latest_time_req.text, "%Y-%m-%dT%H:%M:%S%z") # タイムゾーン込みで日時文字列をdatetime型へ
@@ -33,12 +40,6 @@ def weather():
     detail_area = "130010" # 詳細の予報エリア番号 - 今回は東京地方
     stnid = "44132" # 観測所番号 - 今回は千代田区 北の丸公園
 
-    # 天気概況
-    overview_forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{area}.json"
-    overview_forecast_req = requests.get(overview_forecast_url)
-    overview_forecast_data = overview_forecast_req.json() # 天気概況
-    overview_forecast_text = "\n".join(overview_forecast_data["text"].split())
-
     # 天気予報
     forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area}.json"
     forecast_req = requests.get(forecast_url)
@@ -46,7 +47,8 @@ def weather():
     forecast_data = forecast_data[0]["timeSeries"][0]["areas"] #エリア毎の予報データ（天気, 風速, 風向...etc）が格納
     forecast_data_target_index = find_index(forecast_data, detail_area)
     weathers = forecast_data[forecast_data_target_index]["weathers"] # 天気
-    tommorow_weather = " ".join(weathers[1].split())
+    today_weather = weathers[0]
+    tommorow_weather = weathers[1]
 
     # アメダス
     amedas_url = f"https://www.jma.go.jp/bosai/amedas/data/point/{stnid}/{yyyymmdd}_{h3}.json"
@@ -54,13 +56,5 @@ def weather():
     amedas_data = amedas_req.json()
     latest_key = max(amedas_data) # 最新のアメダスデータが入っているkey
     latest_temp = confirm_aqc(amedas_data[latest_key]["temp"]) # 最新の気温データを取得, 品質情報を確認
-    latest_precipitation10m = confirm_aqc(amedas_data[latest_key]["precipitation10m"]) # 最新の降水量データを取得, 品質情報を確認
 
-    #print(f"現在の気温 : {latest_temp} 度")
-    #print(f"現在の降水量(10分あたり) : {latest_precipitation10m} mm")
-    #print(f"翌日の天気: {tommorow_weather}")
-    #print(f"天気概況 : {overview_forecast_text}")
-
-    return weathers[0], latest_temp
-
-#print(weather())
+    return today_weather,tommorow_weather,latest_temp
