bdc5e222afa2e3469a316b4a9745993ce16a3a5d
hedwig100 <pentagon7951@outlook.jp>
2022-04-22 14:24:41 +0900

    server.pyを修正

--- src/server.py
+++ src/server.py
@@ -1,10 +1,12 @@
 import logging,time
 from gpiozero import Button
-import weather
+from signal import pause
 
-# TODO: 今度ラズパイを触る時にピンの番号を指定する。
-BUTTON_S2T_PIN = 10
-BUTTON_WEATHER_PIN = 11
+import weather
+import slack
+
+BUTTON_S2T_PIN = 6
+BUTTON_WEATHER_PIN = 20
 
 def init_logging():
     """init_logging
@@ -17,27 +19,30 @@ def init_logging():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
     return logging.getLogger(name="server")
 
-def main():
-    logger = init_logging()
+logger = init_logging()
 
+def weather_call():
+    logger.info("weather!")
+    today_weather,tommorow_weather,tmperature = weather.weather()
+    logger.info(f"本日の天気 : {today_weather},本日の気温: {tmperature}度,明日の天気: {tommorow_weather}")
+    # TODO:displayで電光掲示板に表示する
+    slack.message_send_test.send_message(f"本日の天気 : {today_weather},本日の気温: {tmperature}度,明日の天気: {tommorow_weather}")
+
+def speech_call():
+    logger.info("speech!")
+    # TOOD:録音を開始する関数を呼ぶ
+    # TODO:voc2txtを使って認識結果を得る
+    # TODO:displayで電光掲示板に表示する
+    # TODO:slackに通知する
+
+def main():
     button_s2t = Button(BUTTON_S2T_PIN)
     button_weather = Button(BUTTON_WEATHER_PIN)
 
     logger.info("server started")
-    while True:
-        if button_s2t.when_pressed:
-            # TOOD:録音を開始する関数を呼ぶ
-            # TODO:voc2txtを使って認識結果を得る
-            # TODO:displayで電光掲示板に表示する
-            # TODO:slackに通知する
-            pass 
-        elif button_weather.when_pressed:
-            today_weather,tommorow_weather,tmperature = weather.weather()
-            logger.info(f"本日の天気 : {today_weather},本日の気温: {tmperature}度,明日の天気: {tommorow_weather}")
-            # TODO:displayで電光掲示板に表示する
-            # TODO:slackに通知する
-        else:
-            time.sleep(0.05)
+    button_s2t.when_pressed = speech_call
+    button_weather.when_pressed = weather_call
+    pause()
 
 if __name__ == "__main__":
     main()
